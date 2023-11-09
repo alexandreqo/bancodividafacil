5874ae27d692cd546d3c60b242d4c14a81f7308e
Alexandre Queiroz de Oliveira <alexandre@gmail.com>
2023-11-09 13:36:03 -0300

    init

---
+++ src/main/java/com/bancodividafacil/dividafacil/DividafacilApplication.java
@@ -0,0 +1,13 @@
+package com.bancodividafacil.dividafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividafacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancodividafacil/dividafacil/DividafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodividafacil.dividafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
